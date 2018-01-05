ac02a536c3b843bac420840735854566c47b995a
warren fung <wfung@e1z2r6p1.42.us.org>
2018-01-04 16:30:21 -0800

    uh hope new translate math is correct

--- translate.c
+++ translate.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 19:53:07 by wfung             #+#    #+#             */
-/*   Updated: 2017/11/28 18:35:36 by wfung            ###   ########.fr       */
+/*   Updated: 2018/01/04 16:23:31 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,29 @@ void	translate(t_env *e)
 	int		j;
 	int		k = 0;
 	int		p = 0;
+	float	new_x_mid;
+	float	new_y_mid;
+
+	new_x_mid = (e->pts[e->row - 1][e->col - 1].x - e->pts[0][0].x) / 2;
+	new_y_mid = (e->pts[e->row - 1][e->col - 1].y - e->pts[0][0].y) / 2;
 	i = 0;
-	printf("w_m_y %f w_m_x %f e_x %f e_y %f\n", e->win_mid_y, e->win_mid_x / 2, e->end_x, e->end_y / 2);
+	printf("\ntranslation x = [%f] y = [%f]\n", new_x_mid, new_y_mid);
 	while (i < e->row)
 	{
 		j = 0;
 		while (j < e->col)
 		{
+			/*
 			e->pts[i][j].x = e->pts[i][j].x + e->win_mid_x - (e->end_x / 2);
 			e->pts[i][j].y = e->pts[i][j].y + e->win_mid_y - (e->end_y / 2);
+			*/
+			e->pts[i][j].x += (e->pts[e->row - 1][e->col - 1].x - e->pts[0][0].x) / 2;
+			e->pts[i][j].y += (e->pts[e->row - 1][e->col - 1].y - e->pts[0][0].y) / 2;
 			j++;
 		}
 		i++;
 	}
+	printf("\nprinting new points\n");
 	while (k < e->row)
 	{
 		p = 0;
